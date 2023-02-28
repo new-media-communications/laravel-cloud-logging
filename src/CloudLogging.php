37d51b3e836a232a03fbce87acaaf4d49b23d93c
Fitim Vata <fitimvata28@gmail.com>
2023-02-28 14:55:54 +0100

    wip

--- src/CloudLogging.php
+++ src/CloudLogging.php
@@ -3,8 +3,10 @@
 namespace Nmc\CloudLogging;
 
 use Google\Cloud\Logging\LoggingClient;
+use Illuminate\Support\Arr;
 use Monolog\Handler\PsrHandler;
 use Monolog\Logger;
+use Nmc\CloudLogging\Google\PsrLogger;
 use Psr\Log\LoggerInterface;
 
 class CloudLogging
@@ -17,12 +19,44 @@ class CloudLogging
         $credentialsKey = is_array($credentials) ? 'keyFile' : (is_string($config['credentials']) ? 'keyFilePath' : 'keyFile');
         $credentialsValue = is_array($credentials) ? $credentials : $config['credentials'];
 
-        $logger = LoggingClient::psrBatchLogger($name, array_merge([
+        $options = array_merge([
             'clientConfig' => [
                 'projectId' => $config['project'],
                 $credentialsKey => $credentialsValue,
             ],
-        ], $config['client_config'] ?? []));
+        ], $config['client_config'] ?? []);
+
+        $options['batchEnabled'] = true;
+
+
+        $client = new LoggingClient($options['clientConfig']);
+
+
+        $messageKey = null;
+
+        if (isset($options['messageKey'])) {
+            $messageKey = $options['messageKey'];
+            unset($options['messageKey']);
+        }
+
+        $psrLoggerOptions = Arr::only($options, [
+            'metadataProvider',
+            'batchEnabled',
+            'debugOutput',
+            'batchOptions',
+            'clientConfig',
+            'batchRunner',
+            'closureSerializer',
+            'debugOutputResource'
+        ]);
+
+        $logger = new PsrLogger(
+            $client->logger($name, $options),
+            $messageKey,
+            $psrLoggerOptions + [
+                'clientConfig' => $options['clientConfig']
+            ]
+        );
 
         return new Logger($name, [new PsrHandler($logger)]);
     }
